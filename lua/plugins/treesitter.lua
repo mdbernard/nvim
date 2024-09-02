c5f9b99bde663667daba48d17883bf5a89051ddb
Mike Bernard <mdbernard@protonmail.com>
2024-09-01 18:38:32 -0700

    auto-install treesitter plugins

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -5,6 +5,7 @@ return {
         local config = require("nvim-treesitter.configs")
         config.setup({
             ensure_installed = { "lua", "python", "rust", "markdown", "vim", "vimdoc", "c" },
+            auto_install = true,
             highlight = { enable = true },
             indent = { enable = true }
         })
