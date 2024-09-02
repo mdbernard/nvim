231a3c98e803d4983fee96197abf8288df8877da
Mike Bernard <mdbernard@protonmail.com>
2024-09-01 20:40:50 -0700

    add snippets and autocompletions

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -3,7 +3,7 @@ return {
         "williamboman/mason.nvim",
         config = function()
             require("mason").setup()
-        end
+        end,
     },
     {
         "williamboman/mason-lspconfig.nvim",
@@ -13,25 +13,36 @@ return {
                     -- Lua
                     "lua_ls",
                     -- Python
+                    "basedpyright",
                     "ruff_lsp",
                     -- Rust
-                    "rust_analyzer"
-                }
+                    "rust_analyzer",
+                },
             })
-        end
+        end,
     },
     {
         "neovim/nvim-lspconfig",
         config = function()
+            local capabilities = require("cmp_nvim_lsp").default_capabilities()
             local lspconfig = require("lspconfig")
 
-            lspconfig.lua_ls.setup({})
-            lspconfig.ruff_lsp.setup({})
-            lspconfig.rust_analyzer.setup({})
+            lspconfig.lua_ls.setup({
+                capabilities = capabilities,
+            })
+            lspconfig.basedpyright.setup({
+                capabilities = capabilities
+            })
+            lspconfig.ruff_lsp.setup({
+                capabilities = capabilities,
+            })
+            lspconfig.rust_analyzer.setup({
+                capabilities = capabilities,
+            })
 
             vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
             vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
             vim.keymap.set({ "n" }, "<leader>ca", vim.lsp.buf.code_action, {})
-        end
-    }
+        end,
+    },
 }
