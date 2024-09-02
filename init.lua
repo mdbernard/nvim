8d7150199c803f225e8a3a0b17a52ed3a8c15bbf
Mike Bernard <mdbernard@protonmail.com>
2024-09-01 19:54:33 -0700

    relative line number toggling

--- init.lua
+++ init.lua
@@ -3,6 +3,10 @@ vim.cmd("set tabstop=4")
 vim.cmd("set softtabstop=4")
 vim.cmd("set shiftwidth=4")
 
+vim.cmd("set relativenumber")
+vim.cmd("autocmd InsertEnter * :set number norelativenumber")
+vim.cmd("autocmd InsertLeave * :set relativenumber")
+
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
