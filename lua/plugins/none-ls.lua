5eaaad0aabd63c39719d9d39f4a049f877f1652b
Mike Bernard <mdbernard@protonmail.com>
2024-09-01 18:53:30 -0700

    add python linting and formatting

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,12 +1,21 @@
 return {
 	"nvimtools/none-ls.nvim",
+    dependencies = {
+        "nvimtools/none-ls-extras.nvim",
+    },
 	config = function()
 		local null_ls = require("null-ls")
 
 		null_ls.setup({
 			sources = {
-				null_ls.builtins.formatting.stylua,
-			},
+				-- Lua
+                null_ls.builtins.formatting.stylua,
+                -- Python
+                null_ls.builtins.diagnostics.mypy,
+			    null_ls.builtins.formatting.isort,
+                require("none-ls.diagnostics.ruff"),
+                require("none-ls.formatting.ruff"),
+            },
 		})
 
 		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
