b311ca48b507741b63c05f88dee27bb546fef123
Mike Bernard <mdbernard@protonmail.com>
2024-09-02 16:40:05 -0700

    add barbar

---
+++ lua/plugins/barbar.lua
@@ -0,0 +1,8 @@
+return {
+    'romgrk/barbar.nvim',
+    dependencies = {
+        'nvim-tree/nvim-web-devicons', -- OPTIONAL: for file icons
+        'lewis6991/gitsigns.nvim' -- OPTIONAL: for git status
+
+    }
+}
