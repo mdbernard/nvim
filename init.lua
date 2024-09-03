90cdee23d8355ad59ab24b8b99c6291e63c1c966
Mike Bernard <mdbernard@protonmail.com>
2024-09-02 21:19:02 -0700

    don't highlight search results after hitting ESC

--- init.lua
+++ init.lua
@@ -1,3 +1,4 @@
+-- Always use 4 spaces instead of a TAB character
 vim.cmd("set expandtab")
 vim.cmd("set tabstop=4")
 vim.cmd("set softtabstop=4")
@@ -5,11 +6,16 @@ vim.cmd("set shiftwidth=4")
 
 vim.cmd("set nowrap")
 
+-- Show relative line numbers in normal mode, absolute in insert mode
 vim.cmd("set relativenumber")
 vim.cmd("autocmd InsertEnter * :set number norelativenumber")
 vim.cmd("autocmd InsertLeave * :set relativenumber")
 
+-- Always leave a buffer of lines at the top and bottom of the screen
 vim.cmd("set scrolloff=8")
+
+-- Stop highlighting search results after hitting ESC
+vim.cmd("nnoremap <esc> :noh<return><esc>")
 
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
