062e4f1209ca5bbc0c84574b29f08ccf8d3fc152
Mike Bernard <mdbernard@protonmail.com>
2024-09-02 21:39:05 -0700

    add spectre search tool

---
+++ lua/plugins/spectre.lua
@@ -0,0 +1,22 @@
+return {
+    "nvim-pack/nvim-spectre",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+    },
+    config = function ()
+        require("spectre")
+
+        vim.keymap.set("n", "<leader>ts", '<cmd>lua require("spectre").toggle()<CR>', {
+            desc = "Toggle search tool (Spectre)"
+        })
+        vim.keymap.set("n", "<leader>sw", '<cmd>lua require("spectre").open_visual({select_word=true})<CR>', {
+            desc = "Search for the currently selected word (normal mode)"
+        })
+        vim.keymap.set("v", "<leader>sw", '<esc><cmd>lua require("spectre").open_visual()<CR>', {
+            desc = "Search for the currently selected word (visual mode)"
+        })
+        vim.keymap.set("n", "<leader>sf", '<cmd>lua require("spectre").open_file_search({select_word=true})<CR>', {
+            desc = "Search for the currently selected word in the current file"
+        })
+    end
+}
