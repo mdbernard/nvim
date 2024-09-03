bbedb61562045936deb09497e44452f80038a9cf
Mike Bernard <mdbernard@protonmail.com>
2024-09-02 20:50:00 -0700

    add fastwrap autopairs

--- lua/plugins/autopairs.lua
+++ lua/plugins/autopairs.lua
@@ -1,15 +1,31 @@
 return {
-	"windwp/nvim-autopairs",
-	event = "InsertEnter",
-	config = function()
-		require("nvim-autopairs").setup({
-			-- do not insert closing pair if already detected on the same line
-			enable_check_line_bracket_next = false,
+    "windwp/nvim-autopairs",
+    dependencies = {
+        "hrsh7th/nvim-cmp",
+    },
+    event = "InsertEnter",
+    config = function()
+        require("nvim-autopairs").setup({
+            -- don't add a pairing if one already exists on the same line
+            enable_check_bracket_line = false,
 
-			-- do not insert closing pair if next character is text
-			ignored_next_char = "[%w%.]",
+            fast_wrap = {
+                map = '<M-e>',
+                chars = { "{", "[", "(", '"', "'", "`", "<" },
+                pattern = [=[[%'%"%>%]%)%}%,]]=],
+                end_key = '$',
+                before_key = 'h',
+                after_key = 'l',
+                cursor_pos_before = true,
+                keys = 'qwertyuiopzxcvbnmasdfghjkl',
+                manual_position = true,
+                highlight = 'Search',
+                highlight_grey = 'Comment'
+            }
+        })
 
-			disable_filetype = { "TelescopePrompt" },
-		})
-	end,
+        local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+        local cmp = require("cmp")
+        cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done { map_char = { tex = "" } })
+    end,
 }
