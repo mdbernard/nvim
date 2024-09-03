8a9d3c0ef7289535840c134332300716316a628e
Mike Bernard <mdbernard@protonmail.com>
2024-09-02 21:21:58 -0700

    tweak keymaps

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,23 +1,24 @@
 return {
 	"nvimtools/none-ls.nvim",
-    dependencies = {
-        "nvimtools/none-ls-extras.nvim",
-    },
+	dependencies = {
+		"nvimtools/none-ls-extras.nvim",
+	},
 	config = function()
 		local null_ls = require("null-ls")
 
 		null_ls.setup({
 			sources = {
 				-- Lua
-                null_ls.builtins.formatting.stylua,
-                -- Python
-                null_ls.builtins.diagnostics.mypy,
-			    null_ls.builtins.formatting.isort,
-                require("none-ls.diagnostics.ruff"),
-                require("none-ls.formatting.ruff"),
-            },
+				null_ls.builtins.formatting.stylua,
+				-- Python
+				null_ls.builtins.diagnostics.mypy,
+				null_ls.builtins.formatting.isort,
+				require("none-ls.diagnostics.ruff"),
+				require("none-ls.formatting.ruff"),
+			},
 		})
 
-		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+		-- df = "do format"
+		vim.keymap.set("n", "<leader>df", vim.lsp.buf.format, {})
 	end,
 }
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -5,8 +5,12 @@ return {
         dependencies = { "nvim-lua/plenary.nvim" },
         config = function()
             local builtin = require("telescope.builtin")
-            vim.keymap.set("n", "<C-p>", builtin.find_files, {})
-            vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+
+            -- ff = "find files"
+            vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
+
+            -- ft = "find text"
+            vim.keymap.set("n", "<leader>ft", builtin.live_grep, {})
         end
     },
     {
