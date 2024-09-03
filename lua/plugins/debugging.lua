3f4d58db2fa31fd7fa5b92a9a263a050f8e06809
Mike Bernard <mdbernard@protonmail.com>
2024-09-02 20:24:13 -0700

    add python debugging

--- lua/plugins/debugging.lua
+++ lua/plugins/debugging.lua
@@ -3,11 +3,13 @@ return {
 	dependencies = {
 		"rcarriga/nvim-dap-ui",
 		"nvim-neotest/nvim-nio",
+        "mfussenegger/nvim-dap-python",
 	},
 	config = function()
 		local dap = require("dap")
 		local dapui = require("dapui")
 
+		-- general setup
 		vim.keymap.set("n", "<Leader>db", dap.toggle_breakpoint, {})
 		vim.keymap.set("n", "<Leader>dc", dap.continue, {})
 
@@ -23,5 +25,11 @@ return {
 		dap.listeners.before.event_exited.dapui_config = function()
 			dapui.close()
 		end
+
+		-- Python debugging
+        -- must `pip install debugpy` in the active virtual environment
+        local dap_python = require("dap-python")
+        dap_python.setup("python")
+        dap_python.test_runner = "pytest"
 	end,
 }
