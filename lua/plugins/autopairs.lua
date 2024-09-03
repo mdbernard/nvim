d6aa7228cc2d9435b781b5a17aa462a205b904bd
Mike Bernard <mdbernard@protonmail.com>
2024-09-02 19:38:19 -0700

    update autopairs config

--- lua/plugins/autopairs.lua
+++ lua/plugins/autopairs.lua
@@ -1,5 +1,15 @@
 return {
-    "windwp/nvim-autopairs",
-    event = "InsertEnter",
-    config = true,
+	"windwp/nvim-autopairs",
+	event = "InsertEnter",
+	config = function()
+		require("nvim-autopairs").setup({
+			-- do not insert closing pair if already detected on the same line
+			enable_check_line_bracket_next = false,
+
+			-- do not insert closing pair if next character is text
+			ignored_next_char = "[%w%.]",
+
+			disable_filetype = { "TelescopePrompt" },
+		})
+	end,
 }
